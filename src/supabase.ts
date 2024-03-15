029e677b9c6d628f64056b80098601c168f87630
Wali-Youssef <117752075+Wali-Youssef@users.noreply.github.com>
2024-03-15 15:39:14 +0100

    test

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,14 +1,26 @@
 import { createClient } from '@supabase/supabase-js'
 import { ref } from 'vue'
-
+import { type Database } from './supabase-types'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey)
 
-const { data } = await supabase.auth.getUser()
-export const user = ref(data.user)
-supabase.auth.onAuthStateChange( (evt,session)=>{
-    user.value = session?.user ?? null
-})
+// @ts-ignore
+globalThis.supabase = supabase
+
+// Création d'une référence réactive pour l'utilisateur
+export const user = ref(null);
+
+// Fonction asynchrone pour récupérer l'utilisateur et s'abonner aux changements d'état d'authentification
+async function fetchUser() {
+  const { data } = await supabase.auth.getUser();
+  user.value = data.user;
+  supabase.auth.onAuthStateChange((event, session) => {
+    user.value = session?.user ?? null;
+  });
+}
+
+// Invoquer fetchUser directement pour initialiser l'état de l'utilisateur
+fetchUser();
